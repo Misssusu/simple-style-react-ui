275ffc6f99da77c19042b9dc4607644307e0e8bf
suyuan <suyuan326@qq.com>
2021-02-21 22:59:31 +0800

    不打包react

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,18 @@ module.exports = {
             template: 'index.html'
         })
     ],
-    performance : {
-        hints : false
-    } 
+    externals: {
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React'
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM'
+        }
+    }
 }
