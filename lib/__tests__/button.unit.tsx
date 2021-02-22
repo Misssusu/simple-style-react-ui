5e1af8d265791e41954e0799ffe2f2195ac1531d
suyuan <suyuan326@qq.com>
2021-02-22 18:29:20 +0800

    测试通过第一个单元测试

---
+++ lib/__tests__/button.unit.tsx
@@ -0,0 +1,10 @@
+import Button from '../button'
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+describe('button',()=>{
+    it('是一个div',()=>{
+        const json = renderer.create(<Button/>).toJSON()
+        expect(json).toMatchSnapshot()
+    })
+})
