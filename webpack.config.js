16c5e95a5618527701378daa65da4f2745d458ae
suyuan <suyuan326@qq.com>
2021-02-22 10:51:56 +0800

    配置webpack dev和prod环境

--- webpack.config.js
+++ webpack.config.js
@@ -20,25 +20,5 @@ module.exports = {
                 loader: 'awesome-typescript-loader'   //将ts转为js
             }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'simple-style-react-ui',
-            template: 'index.html'
-        })
-    ],
-    externals: {
-        react: {
-            commonjs: 'react',
-            commonjs2: 'react',
-            amd: 'react',
-            root: 'React'
-        },
-        'react-dom': {
-            commonjs: 'react-dom',
-            commonjs2: 'react-dom',
-            amd: 'react-dom',
-            root: 'ReactDOM'
-        }
     }
 }
---
+++ webpack.config.prod.js
@@ -0,0 +1,19 @@
+const base = require('./webpack.config')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = Object.assign({}, base, {
+    mode: 'production',
+    externals: {
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React'
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM'
+        }
+    }
+})
