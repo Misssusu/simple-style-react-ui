077c65e79c2798feea4dbf1aac078319ab6d59c7
suyuan <suyuan326@qq.com>
2021-02-23 12:47:56 +0800

    add icon component

---
+++ lib/icon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+interface IconProps {
+    name: string
+}
+const Icon: React.FunctionComponent<IconProps> = (props) => {
+    return(
+        <span>icon</span>
+    )
+}
+
+export default Icon
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Button from './button';
+import Icon from './icon';
 
-ReactDom.render(<Button></Button>,document.body);
+ReactDom.render(
+    <div>
+        <Button/>
+        <Icon name='wechat'/>
+    </div>,
+    document.body);
