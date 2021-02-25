4db3707230ba4081a0fe98defa9c22a55dfcf9eb
suyuan <suyuan326@qq.com>
2021-02-25 11:57:26 +0800

    use enzyme 测试点击事件，并解决 mount does not work in enzyme

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,11 +1,21 @@
 import renderer from "react-test-renderer";
 import Icon from "../icon";
 import React from "react";
+import { mount } from "enzyme";
 
 
 describe('icon',()=>{
     it('是一个 svg',()=>{
-        const json = renderer.create(<Icon/>).toJSON()
+        const json = renderer.create(<Icon name="wechat"/>).toJSON()
         expect(json).toMatchSnapshot()
+    })
+    it('onClick',()=>{
+        let n = 1;
+        const fn = () =>{
+            n = 2;
+        }
+        const component = mount(<Icon name="wechat" onClick={fn} />)
+        component.find('svg').simulate('click')
+        expect(n).toEqual(2)
     })
 })
